636edf5d60c6409e8f8230dbf1ca5d0c3ab5fcd4
Alan Vivona <alan.g.vivona@gmail.com>
2018-12-26 20:45:25 -0300

    improved the compile&link script

--- utils/compile-and-link.fish
+++ utils/compile-and-link.fish
@@ -1,4 +1,16 @@
 set filename $argv[1]
-echo "compiling $filename..."
-nasm -f elf64 $filename -o $filename.o; and ld $filename.o -o $filename.elf64; and rm $filename.o
-echo -n "done!"
+
+echo "compiling $filename in elf64 format..."
+if nasm -f elf64 $filename -o $filename.o
+    if ld $filename.o -o $filename.elf64
+        if rm $filename.o
+            echo -n "done!"
+        else
+            echo "FAIL! : rm $filename.o" 1>&2
+        end
+    else
+        echo "FAIL! : ld $filename.o -o $filename.elf64" 1>&2
+    end
+else 
+    echo  "FAIL! : nasm -f elf64 $filename -o $filename.o" 1>&2
+end
