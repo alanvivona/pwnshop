418bdfb4171e369cde99d2c4325d585ca4f3f050
Alan Vivona <alan.g.vivona@gmail.com>
2018-12-29 12:38:30 -0300

    optimized the 0x003 example and eliminated null bytes

--- src/shellcode/0x03-dynamic-addressing/0x03-dynamic-addressing.asm
+++ src/shellcode/0x03-dynamic-addressing/0x03-dynamic-addressing.asm
@@ -1,12 +1,41 @@
+; this is the original version. it has NULL's in it
+;
+;
+; global _start
+; 
+; _start:
+        ; jmp short pushStringAddress
+        ; payload:
+            ; mov rax, 4    ; syscall write
+            ; mov rbx, 1    ; stdout file descriptor is 1
+            ; pop rcx       ; get the address of the string from the stack
+            ; mov rdx, 5    ; length of the string
+            ; int 0x80
+            ; jmp short end
+        ; pushStringAddress:
+            ; call payload  ; put the address of the string on the stack
+            ; db 'hello\n'
+        ; end:
+
+
 global _start
 
 _start:
         jmp short pushStringAddress
         payload:
-            mov rax, 4    ; syscall write
-            mov rbx, 1    ; stdout file descriptor is 1
+            xor rax, rax       ; zero out rax
+            inc rax       ; rax = 1
+            shl rax, 2    ; rax = 1 * 2 * 2 = 4
+            
+            xor rbx, rbx  ; zero out rbx
+            inc rbx       ; rbx = 1 (stdout file descriptor is 1)
+            
             pop rcx       ; get the address of the string from the stack
-            mov rdx, 5    ; length of the string
+            
+            inc rdx       ; rdx = 1
+            shl rdx, 2    ; rdx = 1 * 2 * 2 = 4 
+            inc rdx       ; length of the string
+            
             int 0x80
             jmp short end
         pushStringAddress:
