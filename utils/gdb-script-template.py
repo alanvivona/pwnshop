cd968416797cca20b9cac9fe5f4aa7812afa00e8
Alan Vivona - Syscall59 <alanvivona@users.noreply.github.com>
2019-07-26 22:26:34 -0300

    Update gdb-script-template.py

--- utils/gdb-script-template.py
+++ utils/gdb-script-template.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
+# dependencies: 
+# - gdb
+# - GEF (https://github.com/hugsy/gef)
+
+# usage :
+# sample binary taken from : https://crackmes.one/crackme/5c79727733c5d4776a837d80
+# gdb --command gdb-script-template.py ../crackmes/Sh4ll9/sh4ll9.bin
+
 import gdb
 
 commands = [
